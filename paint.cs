1c36165d9325eefd5a6e84842b27e60f4edc8bf7
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-09-08 08:34:59 +0200

    Script to handle paint.shader

---
+++ paint.cs
@@ -0,0 +1,23 @@
+﻿//Assign paint script to Main Camera. Then set material with paint.shader
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class paint : MonoBehaviour 
+{
+	public Material material;
+	Vector4 iMouse;
+
+	void Update () 
+	{
+		if (Input.GetMouseButton(0))
+		{
+			iMouse = new Vector4 (Input.mousePosition.x/Screen.width,1.0f-Input.mousePosition.y/Screen.height,-1.0f,-1.0f) ; 
+			material.SetVector("iMouse",iMouse);
+		}
+	}
+	
+	void OnRenderImage (RenderTexture source, RenderTexture destination) 
+	{
+		Graphics.Blit (source, destination, material);
+	}
+}
