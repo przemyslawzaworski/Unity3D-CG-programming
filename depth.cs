22fe46b9ac0f0fc91e371dec3edec077d2f62b6c
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-09-17 21:21:24 +0200

    CS script to execute depth.shader

---
+++ depth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class depth : MonoBehaviour 
+{
+	public Material material;
+	public Camera main_camera;
+	
+	void Start () 
+	{
+		main_camera.depthTextureMode = DepthTextureMode.Depth;
+		main_camera.farClipPlane = 80f; //Draw distance
+	}
+	
+	void OnRenderImage (RenderTexture source, RenderTexture destination) 
+	{
+		Graphics.Blit (source, destination, material);
+	}
+}
