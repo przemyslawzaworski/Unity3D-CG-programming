dc8210119bfc2e2bf815bb06696d959e09a68602
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-09-19 17:08:00 +0200

    First commit.

---
+++ notebook_drawings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class notebook_drawings : MonoBehaviour 
+{
+	public Material material;
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination) 
+	{
+		Graphics.Blit (source, destination, material);
+	}
+}
