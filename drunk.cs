190e9e768c30fc738b005f5e0254d405afd8fee7
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-11-06 13:49:00 +0100

    Load drunk.shader into Unity engine.

---
+++ drunk.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drunk : MonoBehaviour 
+{
+	public Material material;
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination) 
+	{
+		Graphics.Blit (source, destination, material);
+	}
+}
