40abaee73d824dff5276374a59d122b6c2ea52c5
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-11-07 13:11:22 +0100

    Load diagonal_transition.shader into Unity engine

---
+++ diagonal_transition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class diagonal_transition : MonoBehaviour 
+{
+	public GameObject light;
+	public Material material;
+
+	void Update()
+	{
+		light.transform.Rotate(new Vector3(Time.deltaTime*4.0f,0.0f,0.0f)); 
+	}
+	
+	void OnRenderImage (RenderTexture source, RenderTexture destination) 
+	{
+		Graphics.Blit (source, destination, material);
+	}
+}
