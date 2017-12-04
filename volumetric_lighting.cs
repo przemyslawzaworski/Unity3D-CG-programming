06dce15cd1f7d36ace722d1a6e6f7ab563cf6cbc
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-12-04 15:12:52 +0100

    Load volumetric_lighting.shader into Unity Engine

---
+++ volumetric_lighting.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class volumetric_lighting : MonoBehaviour 
+{
+	public Material material;
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination) 
+	{
+		Graphics.Blit (source, destination, material);
+	}
+}
