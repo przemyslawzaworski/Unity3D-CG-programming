059d92d152ce1ca9c7fbfc982c138faa3a6501cb
przemyslawzaworski <przemyslawzaworski@gmail.com>
2017-12-18 21:36:57 +0100

    Load buffer.shader into Unity engine

---
+++ buffer.cs
@@ -0,0 +1,25 @@
+﻿//Load buffer.shader into Unity engine
+using UnityEngine;
+
+public class buffer : MonoBehaviour 
+{
+	RenderTexture A;
+	RenderTexture B;
+	public Material material;	
+
+	void Start() 
+	{ 
+		A = new RenderTexture(1024,1024,0);
+		A.Create();	
+		B = new RenderTexture(1024,1024,0);
+		B.Create();	
+	}
+		
+	void Update()
+	{
+		material.SetTexture("MainTex", A);
+		Graphics.Blit(A,B,material);
+		material.SetTexture("MainTex", B);
+		Graphics.Blit(B,A,material);
+	}
+}
