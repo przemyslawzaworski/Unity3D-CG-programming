b7e5c3b3c119e84f6d7ec45191a84a33162db4c7
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-01-14 22:38:04 +0100

    Load fire.shader into Unity engine

---
+++ fire.cs
@@ -0,0 +1,21 @@
+﻿//Apply directly to Main Camera.
+using UnityEngine;
+
+public class fire : MonoBehaviour 
+{
+	public Material material;
+
+	void Update()
+	{
+		float  x = transform.eulerAngles.x * Mathf.Deg2Rad;
+		float  y = transform.eulerAngles.y * Mathf.Deg2Rad;
+		float  z = transform.eulerAngles.z * Mathf.Deg2Rad;
+		Vector4 angle = new Vector4 (-x,-y,-z,1.0f);
+		material.SetVector("camera",angle);
+	}
+	
+	void OnRenderImage (RenderTexture source, RenderTexture destination) 
+	{
+		Graphics.Blit (source, destination, material);
+	}
+}
