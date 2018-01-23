4ddd953490809c1d74f2e9eb3fd77c7690e0a86e
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-01-24 00:54:04 +0100

    Load debug_vertex_index.shader into engine.

---
+++ debug_vertex_index.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class debug_vertex_index : MonoBehaviour 
+{
+	public Material material;
+	public GameObject gameobject;
+
+	void Start () 
+	{
+		Mesh mesh = gameobject.GetComponent<MeshFilter>().sharedMesh;
+		material.SetInt("amount",(int)mesh.vertices.Length);	
+	}
+}
