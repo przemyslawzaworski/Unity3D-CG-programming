ed441ab70c222acb503989e33d575ccc5e63a8f9
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-04-07 11:44:24 +0200

    Generate map of terrain trees.

---
+++ map_trees.cs
@@ -0,0 +1,42 @@
+﻿//Script with shader generate map of trees painted on selected terrain.
+//To see results, assign script with material to gameobject and set variables.
+//Then create custom render texture,with material unlit,initialization mode realtime,
+//source material,set material with shader("map_trees"), update mode realtime.
+//Finally, to show content of custom render texture, just set it as input texture
+//into any material.
+//Written by Przemyslaw Zaworski
+
+using UnityEngine;
+
+public class map_trees : MonoBehaviour 
+{
+	public Material material;  
+	public Terrain terrain;
+	RenderTexture A;
+	RenderTexture B;
+ 
+	void Start()
+	{
+		TerrainData terrain_data = terrain.GetComponent<Terrain>().terrainData;
+		A = new RenderTexture(1024,1024,0);
+		A.Create();  
+		B = new RenderTexture(1024,1024,0);
+		B.Create();
+
+		foreach(TreeInstance tree in terrain_data.treeInstances)
+		{ 
+			Vector4 point = new Vector4 (tree.position.x,tree.position.z,0.0f,0.0f);
+			material.SetVector("_tree", point);	
+			material.SetTexture("_map", A);
+			Graphics.Blit(A,B,material);	
+			material.SetTexture("_map", B);
+			Graphics.Blit(B,A,material);
+		}
+	}
+	
+	void OnDestroy()
+	{
+		A.Release();
+		B.Release();
+	}
+}
