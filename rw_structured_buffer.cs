6cd6fc14f70585208064efcd13b6a95521cd0b12
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-05-01 19:26:01 +0200

    RWStructuredBuffer in vertex shader.

---
+++ rw_structured_buffer.cs
@@ -0,0 +1,42 @@
+﻿// https://github.com/przemyslawzaworski
+// Set plane position (Y=0.0)
+// Collider is generated from vertex shader, using RWStructuredBuffer to transfer data.
+
+using UnityEngine;
+
+public class rw_structured_buffer : MonoBehaviour 
+{
+	public Material material;
+	public GameObject plane;
+	ComputeBuffer compute_buffer;
+	Mesh mesh;
+	Vector3[] data;
+
+	void Start()
+	{
+		mesh = plane.GetComponent<Collider>().GetComponent<MeshFilter>().sharedMesh;
+		data = mesh.vertices;
+		compute_buffer = new ComputeBuffer(data.Length, sizeof(float)*3, ComputeBufferType.Default);
+	}
+
+	void Update() 
+	{
+		Graphics.ClearRandomWriteTargets();
+		material.SetPass(0);
+		material.SetBuffer("data", compute_buffer);
+		Graphics.SetRandomWriteTarget(1, compute_buffer,false);
+		compute_buffer.GetData(data);
+		if (data!=null && plane.GetComponent<Renderer>().isVisible && Time.frameCount % 2 == 0)
+		{
+			mesh.vertices = data;
+			DestroyImmediate(plane.GetComponent<MeshCollider>());
+			MeshCollider mesh_collider = plane.AddComponent<MeshCollider>();
+			mesh_collider.sharedMesh = mesh;
+		}
+	}
+
+	void OnDestroy()
+	{
+		compute_buffer.Dispose();
+	}
+}
---
+++ rw_structured_buffer.shader
@@ -0,0 +1,53 @@
+﻿//https://github.com/przemyslawzaworski
+//Assign displacement map (R) to properties.
+
+Shader "RW Structured Buffer"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "black" {}
+	}
+	Subshader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vertex_shader
+			#pragma fragment pixel_shader
+			#pragma target 5.0
+
+			sampler2D _MainTex;
+			uniform RWStructuredBuffer<float3> data : register(u1);
+
+			struct APPDATA
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				uint id : SV_VertexID;		
+			};
+
+			struct SHADERDATA
+			{
+				float4 vertex : SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			SHADERDATA vertex_shader (APPDATA IN)
+			{
+				SHADERDATA vs;
+				IN.vertex.y = 0.0-tex2Dlod(_MainTex,float4(IN.uv,0,0)).r*(sin(_Time.g)*0.5+0.5);
+				data[IN.id] = IN.vertex.xyz;
+				vs.vertex = UnityObjectToClipPos(IN.vertex);
+				vs.uv = IN.uv;
+				return vs;
+			}
+
+			float4 pixel_shader (SHADERDATA ps) : SV_TARGET
+			{
+				return tex2D(_MainTex,ps.uv); 
+			}
+
+			ENDCG
+		}
+	}
+}
