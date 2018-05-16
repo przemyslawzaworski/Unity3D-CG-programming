c39ef5d0dc4d5058f18f3797749f5195898a9bfd
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-05-16 18:53:16 +0200

    Mesh-Plane collision detection

---
+++ collision_mesh_plane.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class collision_mesh_plane : MonoBehaviour 
+{
+	public ComputeShader shader;
+	public GameObject mesh;
+	public GameObject plane;
+	public Material material;
+	Vector3[] vertices;
+	ComputeBuffer input;	
+	ComputeBuffer output;
+	int handle_init;	
+	int handle_main;	
+	float[] result;
+	string caption;
+	
+	void Start () 
+	{
+		handle_init = shader.FindKernel("CSInit");
+		handle_main = shader.FindKernel("CSMain");
+		vertices = mesh.GetComponent<MeshFilter>().sharedMesh.vertices;
+		input = new ComputeBuffer (vertices.Length, 12, ComputeBufferType.Default);
+		shader.SetBuffer (handle_main, "input", input);
+		input.SetData(vertices);
+		output = new ComputeBuffer (1, 4, ComputeBufferType.Default);
+		shader.SetBuffer (handle_main, "output", output);
+		shader.SetBuffer (handle_init, "output", output);
+		result = new float[1];
+	}
+	
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.Keypad4) ) mesh.transform.Translate (new Vector3(-0.01f,0.0f,0.0f));		
+		if (Input.GetKey(KeyCode.Keypad6) ) mesh.transform.Translate (new Vector3(0.01f,0.0f,0.0f));
+		if (Input.GetKey(KeyCode.Keypad7) ) mesh.transform.Translate (new Vector3(0.00f,0.01f,0.0f));		
+		if (Input.GetKey(KeyCode.Keypad9) ) mesh.transform.Translate (new Vector3(0.0f,-0.01f,0.0f));	
+		if (Input.GetKey(KeyCode.Keypad8) ) mesh.transform.Translate (new Vector3(0.00f,0.00f,0.01f));		
+		if (Input.GetKey(KeyCode.Keypad5) ) mesh.transform.Translate (new Vector3(0.0f,0.00f,-0.01f));
+		Vector3 A = plane.transform.TransformPoint(new Vector3(10.0f,0.0f,10.0f));
+		Vector3 B = plane.transform.TransformPoint(new Vector3(10.0f,0.0f,0.0f));	
+		Vector3 C = plane.transform.TransformPoint(new Vector3(0.0f,0.0f,0.0f));
+		shader.SetMatrix("ObjectToWorld",mesh.GetComponent<Renderer>().localToWorldMatrix);	
+		shader.SetVector("A",A);
+		shader.SetVector("B",B);
+		shader.SetVector("C",C);
+		material.SetVector("A",A);
+		material.SetVector("B",B);
+		material.SetVector("C",C);
+		shader.Dispatch(handle_init, vertices.Length/8, 1, 1);		
+		shader.Dispatch (handle_main, vertices.Length/8, 1, 1);
+		output.GetData(result);		
+		caption=(result[0]>0) ? "Collision detected !" : "";
+	}
+	
+    void OnGUI()
+    {
+		GUIStyle gui_style = new GUIStyle();
+		gui_style.fontSize = 30;
+        GUI.Label(new Rect(20, 50, 200,100), caption, gui_style);
+    }
+	
+	void OnDestroy() 
+	{
+		input.Release();		
+		output.Release();
+	}
+}
---
+++ collision_mesh_plane.shader
@@ -0,0 +1,64 @@
+﻿Shader "Collision Mesh - Plane"
+{
+	Subshader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vertex_shader
+			#pragma fragment pixel_shader
+			#pragma target 5.0
+
+			uniform float4 A;
+			uniform float4 B;
+			uniform float4 C;
+			
+			struct Plane 
+			{
+				float3 n; 
+				float d; 
+			};
+
+			Plane ComputePlane(float3 a, float3 b, float3 c)
+			{
+				Plane p;
+				p.n = normalize(cross(b - a, c - a));
+				p.d = dot(p.n, a);
+				return p;
+			}
+
+			float DistPointPlane(float3 q, Plane p)
+			{
+				return dot(q, p.n) - p.d; 
+			}
+			
+			struct SHADERDATA
+			{
+				float4 vertex : SV_POSITION;
+				float2 uv : TEXCOORD0;
+				float dist : TEXCOORD1;
+			};
+			
+			SHADERDATA vertex_shader (float4 vertex:POSITION, float2 uv:TEXCOORD0)
+			{
+				SHADERDATA vs;
+				vs.vertex = UnityObjectToClipPos (vertex);
+				vs.uv = uv;
+				Plane plane = ComputePlane(A.xyz,B.xyz,C.xyz);   
+				vs.dist = DistPointPlane(mul(unity_ObjectToWorld,vertex),plane);
+				return vs;
+			}
+
+			float4 pixel_shader (SHADERDATA ps) : SV_TARGET
+			{	
+				if (abs(ps.dist)<0.001)
+					return float4(0,0,1,1);
+				else 
+					return float4(0,0,0,1);
+			}
+
+			ENDCG
+
+		}
+	}
+}
