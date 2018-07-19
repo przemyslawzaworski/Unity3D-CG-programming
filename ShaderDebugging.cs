0927d939f58bd27caf87f6b298c6a121829f91ea
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2018-07-19 17:07:28 +0200

    Shader Debugging

---
+++ ShaderDebugging.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class ShaderDebugging : MonoBehaviour 
+{
+	public GameObject target;
+	
+	private Material material;	
+	private ComputeBuffer buffer;
+	private Vector4[] element;
+	private string label;
+	private Renderer render;
+	
+	void Load ()
+	{
+		buffer = new ComputeBuffer(1, 16, ComputeBufferType.Default);
+		element = new Vector4[1];
+		label = string.Empty;
+		render = target.GetComponent<Renderer>(); 
+		material = render.material;
+	}
+	
+	void Start () 
+	{
+		Load();
+	}
+	
+	void Update () 
+	{
+		Graphics.ClearRandomWriteTargets();
+		material.SetPass(0);
+		material.SetBuffer("buffer", buffer);
+		Graphics.SetRandomWriteTarget(1, buffer, false);
+		buffer.GetData(element);
+		label = (element!=null && render.isVisible) ? element[0].ToString("F3") : string.Empty;	
+	}
+
+	void OnGUI()
+	{
+		GUIStyle style = new GUIStyle();
+		style.fontSize = 32;
+		GUI.Label(new Rect(50, 50, 400, 100), label, style);
+	}
+	
+	void OnDestroy()
+	{
+		buffer.Dispose();
+	}
+}
---
+++ ShaderDebugging.shader
@@ -0,0 +1,28 @@
+﻿Shader "Shader Debugging"
+{
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vertex_shader
+			#pragma fragment pixel_shader
+			#pragma target 4.5
+			
+			RWStructuredBuffer<float4> buffer : register(u1);
+			
+			void vertex_shader (inout float4 vertex:POSITION,inout float2 uv:TEXCOORD0)
+			{
+				vertex = UnityObjectToClipPos(vertex);
+			}
+			
+			float4 pixel_shader (float4 vertex:POSITION,float2 uv:TEXCOORD0) : SV_TARGET
+			{
+				float4 p = float4(sin(_Time.g)*0.5+0.5,0.2,0.321,0.789);   //set custom variable
+				buffer[0] = p;   //write value to buffer
+				return lerp(0..xxxx, float4(uv,0,1), step(uv.x,p.x));
+			}
+			ENDCG
+		}
+	}
+}
