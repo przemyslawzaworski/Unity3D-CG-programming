bcb80b60e1eccd4c1d9288da920a7c324dc9d323
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2019-03-10 18:49:50 +0100

    GPU physics

---
+++ Physics/SpherePhysics/SpherePhysics.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+
+public class SpherePhysics : MonoBehaviour
+{
+	public struct Sphere
+	{
+		public Vector3 position;
+		public Vector3 velocity;
+		public float radius;
+		public float massInverse;
+	}
+
+	public int instanceCount = 5000;
+	public Mesh instanceMesh;
+	public Material instanceMaterial;   
+	public ComputeShader computeShader;
+	public float gravity = 9.8f;
+	public Vector3 force = Vector3.zero;
+	public Bounds worldBounds = new Bounds(new Vector3(0, 0, 0), new Vector3(10, 10, 10));
+	public float drag = 0.1f;	
+
+	private int cachedInstanceCount = -1;
+	private int cachedSubMeshIndex = -1;
+	private ComputeBuffer SphereBuffer;
+	private ComputeBuffer SpherePropsBuffer;
+	private ComputeBuffer argsBuffer;
+	private uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+	private int subMeshIndex = 0;
+	private Sphere[] entities;
+
+	void Start()
+	{
+		argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+		UpdateBuffers();
+	}
+
+	void Update()
+	{
+		if (cachedInstanceCount != instanceCount || cachedSubMeshIndex != subMeshIndex)
+			UpdateBuffers();
+		computeShader.SetFloat("DeltaTime", Time.deltaTime);
+		computeShader.SetVector("ExternalForce", new Vector3(force.x, -gravity + force.z, force.y));
+		computeShader.SetVector("WorldBoundsMin", worldBounds.min);
+		computeShader.SetVector("WorldBoundsMax", worldBounds.max);
+		computeShader.SetFloat("DragCoefficient", drag);
+		computeShader.SetInt("Count", instanceCount);
+		computeShader.SetBuffer(0, "SphereBuffer", SphereBuffer);
+		computeShader.Dispatch(0, instanceCount / 64 + 1, 1, 1);
+		instanceMaterial.SetBuffer("SphereBuffer", SphereBuffer);
+		instanceMaterial.SetBuffer("SpherePropsBuffer", SpherePropsBuffer);
+		Graphics.DrawMeshInstancedIndirect(instanceMesh, subMeshIndex, instanceMaterial, worldBounds, argsBuffer);
+	}
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(265, 20, 200, 30), "Instance Count: " + instanceCount.ToString());
+		instanceCount = (int)GUI.HorizontalSlider(new Rect(25, 20, 200, 30), (float)instanceCount, 1.0f, 50000.0f);
+	}
+
+	void UpdateBuffers()
+	{
+		if (instanceMesh != null)
+			subMeshIndex = Mathf.Clamp(subMeshIndex, 0, instanceMesh.subMeshCount - 1);
+		if (SphereBuffer != null)
+			SphereBuffer.Release();
+    
+		SphereBuffer = new ComputeBuffer(instanceCount, (3 + 3 + 1 + 1) * sizeof(float));
+		entities = new Sphere[instanceCount];
+		for (int i = 0; i < instanceCount; i++)
+		{
+			entities[i].position = Random.insideUnitSphere * 7.0f;
+			entities[i].velocity = Random.insideUnitSphere;
+			entities[i].radius = Random.Range(0.05f, 0.5f);
+			entities[i].massInverse = 1.0f / ((4.0f / 3.0f) * Mathf.PI * Mathf.Pow(entities[i].radius, 3));
+		}
+		SphereBuffer.SetData(entities);
+		instanceMaterial.SetBuffer("SphereBuffer", SphereBuffer);
+		if (SpherePropsBuffer != null)
+			SpherePropsBuffer.Release();
+		SpherePropsBuffer = new ComputeBuffer(instanceCount, 4 * sizeof(float));
+		Vector4[] props = new Vector4[instanceCount];
+		for (int i = 0; i < instanceCount; i++)
+		{
+			props[i].x = 0.5f + 0.5f * Random.value;
+			props[i].y = 0.5f + 0.5f * Random.value;
+			props[i].z = 0.5f + 0.5f * Random.value;
+			props[i].w = Random.value;
+		}
+		SpherePropsBuffer.SetData(props);
+		instanceMaterial.SetBuffer("SpherePropsBuffer", SpherePropsBuffer);
+		if (instanceMesh != null)
+		{
+			args[0] = (uint)instanceMesh.GetIndexCount(subMeshIndex);
+			args[1] = (uint)instanceCount;
+			args[2] = (uint)instanceMesh.GetIndexStart(subMeshIndex);
+			args[3] = (uint)instanceMesh.GetBaseVertex(subMeshIndex);
+		}
+		else
+		{
+			args[0] = args[1] = args[2] = args[3] = 0;
+		}
+		argsBuffer.SetData(args);
+		cachedInstanceCount = instanceCount;
+		cachedSubMeshIndex = subMeshIndex;
+	}
+
+	void OnDisable()
+	{
+		SphereBuffer.Release();
+		SpherePropsBuffer.Release();
+		argsBuffer.Release();
+	}
+}
---
+++ Physics/SpherePhysics/SpherePhysics.shader
@@ -0,0 +1,65 @@
+﻿Shader "SpherePhysics" 
+{
+	Properties 
+	{
+		_Metallic ("Metallic", Range(0,1)) = 1.0	
+		_Smoothness ("Smoothness", Range(0,1)) = 0.5
+		[HideInInspector] _texcoord( "", 2D ) = "white" {}
+	}
+	SubShader 
+	{
+		Tags { "RenderType"="Opaque" }      
+		CGPROGRAM
+		#pragma surface SurfaceShader Standard fullforwardshadows addshadow
+		#pragma instancing_options procedural:setup
+		
+		float _Smoothness, _Metallic;
+
+		struct Input 
+		{
+			float2 uv_texcoord;
+		};
+
+		struct Sphere
+		{
+			float3 position;
+			float3 velocity;
+			float radius;
+			float massInverse;
+		};
+
+		#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+			StructuredBuffer<Sphere> SphereBuffer;
+			StructuredBuffer<float4> SpherePropsBuffer;
+		#endif
+
+		void setup()
+		{
+			#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+				Sphere sphere = SphereBuffer[unity_InstanceID];
+				float s = sphere.radius * 2.0;
+				unity_ObjectToWorld._11_21_31_41 = float4(s, 0, 0, 0);
+				unity_ObjectToWorld._12_22_32_42 = float4(0, s, 0, 0);
+				unity_ObjectToWorld._13_23_33_43 = float4(0, 0, s, 0);
+				unity_ObjectToWorld._14_24_34_44 = float4(sphere.position, 1);
+				unity_WorldToObject = unity_ObjectToWorld;
+				unity_WorldToObject._14_24_34 *= -1;
+				unity_WorldToObject._11_22_33 = 1.0f / unity_WorldToObject._11_22_33;
+			#endif
+		}
+
+		void SurfaceShader (Input IN, inout SurfaceOutputStandard o) 
+		{         
+			o.Albedo = 1;
+			o.Metallic = _Metallic;
+			o.Smoothness = _Smoothness;
+			o.Alpha = 1.0;
+			#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+				o.Albedo *= SpherePropsBuffer[unity_InstanceID].xyz;
+				o.Smoothness *= SpherePropsBuffer[unity_InstanceID].w;
+			#endif
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
