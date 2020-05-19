5712818f9bb390ee3a456468ed901d2944a9e547
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2020-05-19 13:18:55 +0200

    Depth Buffer To World Space conversion

---
+++ DepthBufferToWorldSpace.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class DepthBufferToWorldSpace : MonoBehaviour
+{
+	public Shader DepthBufferToWorldSpaceShader;
+
+	private Camera _MainCamera;	
+	private Material _Material;
+
+	void Start()
+	{
+		_Material = new Material(DepthBufferToWorldSpaceShader);
+		_MainCamera = GetComponent<Camera>();
+		_MainCamera.depthTextureMode = DepthTextureMode.Depth;
+	}
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination) 
+	{
+		Matrix4x4 m = GL.GetGPUProjectionMatrix(_MainCamera.projectionMatrix, false);
+		m[2, 3] = m[3, 2] = 0.0f; m[3, 3] = 1.0f;
+		Matrix4x4 ProjectionToWorld = Matrix4x4.Inverse(m * _MainCamera.worldToCameraMatrix) * Matrix4x4.TRS(new Vector3(0, 0, -m[2,2]), Quaternion.identity, Vector3.one);
+		_Material.SetMatrix("unity_ProjectionToWorld", ProjectionToWorld);
+		Graphics.Blit (source, destination, _Material);
+	}
+}
---
+++ DepthBufferToWorldSpace.shader
@@ -0,0 +1,31 @@
+﻿Shader "DepthBufferToWorldSpace"
+{
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex VSMain
+			#pragma fragment PSMain
+			#pragma target 5.0
+
+			float4x4 unity_ProjectionToWorld;
+			sampler2D _CameraDepthTexture;
+
+			void VSMain (inout float4 vertex:POSITION, inout float2 uv:TEXCOORD0, out float3 direction:TEXCOORD1)
+			{
+				vertex = UnityObjectToClipPos(vertex);
+				direction = mul(unity_ProjectionToWorld, float4(vertex.xy, 0.0, 1.0)) - _WorldSpaceCameraPos;
+			}
+
+			void PSMain (float4 vertex:POSITION, float2 uv:TEXCOORD0, float3 direction:TEXCOORD1, out float4 fragColor:SV_TARGET)
+			{
+				float depth = 1.0 / (_ZBufferParams.z * tex2D(_CameraDepthTexture, uv.xy) + _ZBufferParams.w);
+				float3 worldspace = direction * depth + _WorldSpaceCameraPos;
+				fragColor = float4(worldspace, 1.0);
+			}
+
+			ENDCG
+		}
+	}
+}
