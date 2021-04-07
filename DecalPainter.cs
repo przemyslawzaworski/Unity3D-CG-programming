4f5e34466b9fe0060ba1f22750bc0fd9ac2a5ddb
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2021-04-07 21:52:23 +0200

    Decal projector minimal example

---
+++ DecalPainter.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class DecalPainter : MonoBehaviour
+{
+	public GameObject TargetObject;
+	public Shader DecalPainterShader;
+	public Texture2D DecalTexture;
+	public bool Debug = true;
+	
+	private Matrix4x4 _DecalViewMatrix, _DecalProjectionMatrix;
+	private Material _TargetMaterial;
+	private Material _PaintMaterial;  //unwrapped mesh
+	private Mesh _TargetMesh;	
+	private Renderer _TargetRenderer;
+	private RenderTexture _RenderTexture;	
+
+	void Start()
+	{
+		_RenderTexture = new RenderTexture(4096, 4096, 0);
+		_RenderTexture.Create();
+		_PaintMaterial = new Material(DecalPainterShader);
+		_TargetRenderer = TargetObject.GetComponent<Renderer>();
+		_TargetMesh = TargetObject.GetComponent<MeshFilter>().sharedMesh;
+		_TargetMaterial = _TargetRenderer.sharedMaterial;
+	}
+
+	void Update()
+	{
+		_PaintMaterial.SetMatrix("_MeshModelMatrix", _TargetRenderer.localToWorldMatrix);
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit))
+		{
+			_DecalViewMatrix = Matrix4x4.TRS(hit.point + hit.normal, Quaternion.LookRotation(-hit.normal, Vector3.up), Vector3.one).inverse;
+			_DecalProjectionMatrix = Matrix4x4.Ortho(-0.2f, 0.2f, -0.2f, 0.2f, 0.01f, 1.0f);
+			_PaintMaterial.SetMatrix("_DecalViewMatrix", _DecalViewMatrix);
+			_PaintMaterial.SetMatrix("_DecalProjectionMatrix", _DecalProjectionMatrix);
+			_PaintMaterial.SetTexture("_DecalTexture", DecalTexture);
+		}		
+		RenderTexture currentRT = RenderTexture.active;
+		RenderTexture.active = _RenderTexture;
+		GL.Clear(false, true, Color.black, 1.0f);
+		_PaintMaterial.SetPass(0);
+		Graphics.DrawMeshNow(_TargetMesh, Vector3.zero, Quaternion.identity);
+		RenderTexture.active = currentRT;
+		_TargetMaterial.mainTexture = _RenderTexture;
+	}
+
+	void OnGUI()
+	{
+		if (Debug)
+			GUI.DrawTexture(new Rect(0, 0, 512, 512), _RenderTexture, ScaleMode.ScaleToFit);
+	}
+
+	void OnDestroy()
+	{
+		_RenderTexture.Release();
+		Destroy(_PaintMaterial);
+	}
+}
---
+++ DecalPainter.shader
@@ -0,0 +1,36 @@
+﻿Shader "DecalPainter"
+{
+	SubShader
+	{
+		Pass
+		{
+			ZTest Off
+			ZWrite Off
+			Cull Off
+			CGPROGRAM
+			#pragma vertex VSMain
+			#pragma fragment PSMain
+
+			float4x4 _MeshModelMatrix, _DecalViewMatrix, _DecalProjectionMatrix;
+			sampler2D _DecalTexture;
+
+			void VSMain (inout float4 vertex:POSITION, inout float2 uv:TEXCOORD0, out float4 worldPos:TEXCOORD1, out float4 clipPos:TEXCOORD2)
+			{
+				worldPos = mul(_MeshModelMatrix, vertex);
+				clipPos = mul(_DecalViewMatrix, worldPos);
+				clipPos = mul(_DecalProjectionMatrix, clipPos);	
+				float2 texcoord = uv.xy;
+				texcoord.y = 1.0 - texcoord.y; 
+				texcoord = texcoord * 2.0 - 1.0;
+				vertex = float4(texcoord, 0.0, 1.0);
+			}
+
+			float4 PSMain (float4 vertex:POSITION, float2 uv:TEXCOORD0, float4 worldPos:TEXCOORD1, float4 clipPos:TEXCOORD2) : SV_TARGET
+			{
+				float2 tc = float2( clipPos.x / clipPos.w, clipPos.y / clipPos.w) / 2.0 + 0.5;
+				return (tc.x > 0.0 && tc.x < 1.0 && tc.y > 0.0 && tc.y < 1.0) ? tex2D(_DecalTexture, tc) : float4(1, 0, 0, 1);
+			}
+			ENDCG
+		}
+	}
+}
