006ac828f67fd42a454c3f2b9e8621d988323906
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2021-08-19 23:16:54 +0200

    Tessellation from SV_VertexID

---
+++ VertexShaderTessellation.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[RequireComponent (typeof(Camera))]
+public class VertexShaderTessellation : MonoBehaviour
+{
+	public Shader TessellationShader;
+	[Range(1, 1024)] public int TessellationFactor = 5;
+	public UnityEngine.Rendering.CullMode CullMode = UnityEngine.Rendering.CullMode.Off;
+
+	ComputeBuffer _VertexBuffer;
+	Material _Material;
+	int _VertexCount = 0;
+	Vector4[] _Vertices;
+
+	byte[] ToByteArray(Vector4[] vectors)
+	{
+		byte[] bytes = new byte[sizeof(float) * vectors.Length * 4]; 
+		for (int i = 0; i < vectors.Length * 4; i++)
+			Buffer.BlockCopy(BitConverter.GetBytes(vectors[i / 4][i % 4]), 0, bytes, i*sizeof(float), sizeof(float));
+		return bytes;
+	}
+
+	void Start()
+	{
+		Mesh mesh = Resources.GetBuiltinResource<Mesh>("Quad.fbx");
+		List<Vector4> vertices = new List<Vector4>();
+		for (int i = 0; i < mesh.triangles.Length; i++)
+		{
+			Vector3 p = mesh.vertices[mesh.triangles[i]];
+			vertices.Add(new Vector4(p.x, p.y, p.z, 1.0f));
+		}
+		_Vertices = vertices.ToArray();
+		Camera.main.clearFlags = CameraClearFlags.SolidColor;
+		_Material = new Material(TessellationShader);
+		_VertexBuffer = new ComputeBuffer(4 * _Vertices.Length, sizeof(float), ComputeBufferType.Raw);
+		 byte[] bytes = ToByteArray(_Vertices);
+		_VertexBuffer.SetData(bytes);
+	}
+
+	void OnPreRender()
+	{
+		GL.wireframe = true;
+	}
+
+	void OnPostRender()
+	{
+		_Material.SetBuffer("_VertexBuffer", _VertexBuffer);
+		_Material.SetInt("_TessellationFactor", TessellationFactor);
+		_Material.SetInt("_CullMode", (int)CullMode);
+		_Material.SetPass(0);
+		_VertexCount = TessellationFactor * TessellationFactor * _Vertices.Length;
+		Graphics.DrawProcedural(MeshTopology.Triangles, _VertexCount, 1);
+	}
+
+	void OnGUI()
+	{
+		GL.wireframe = false;
+		GUI.Label(new Rect(10, 10, 200, 30), "Vertex Count: " + _VertexCount.ToString());
+	}
+
+	void OnDestroy()
+	{
+		_VertexBuffer.Release();
+		Destroy(_Material);
+	}
+}
---
+++ VertexShaderTessellation.shader
@@ -0,0 +1,75 @@
+﻿// GPU PRO 3, Advanced Rendering Techniques, A K Peters/CRC Press 2012
+// Chapter 1 - Vertex shader tessellation, Holger Gruen
+
+Shader "Vertex Shader Tessellation"
+{
+	SubShader
+	{
+		Pass
+		{
+			Cull [_CullMode]
+			CGPROGRAM
+			#pragma vertex VSMain
+			#pragma fragment PSMain
+			#pragma target 5.0
+
+			ByteAddressBuffer  _VertexBuffer;
+			int _TessellationFactor;
+
+			float4 VSMain (uint id : SV_VertexID) : SV_POSITION
+			{
+				uint subtriangles = (_TessellationFactor * _TessellationFactor);
+				float triangleID = float (( id / 3 ) % subtriangles);
+				float row = floor (sqrt( triangleID ));
+				uint column = triangleID - ( row * row );
+				float incuv = 1.0 / _TessellationFactor;
+				float u = ( 1.0 + row ) / _TessellationFactor;
+				float v = incuv * floor (float(column) * 0.5);
+				u -= v;
+				float w = 1.0 - u - v;
+				uint address = id / (3u * subtriangles) * 3u;
+				float3 p1 = asfloat(_VertexBuffer.Load4(((address + 0) * 4) << 2)).xyz;
+				float3 p2 = asfloat(_VertexBuffer.Load4(((address + 1) * 4) << 2)).xyz;
+				float3 p3 = asfloat(_VertexBuffer.Load4(((address + 2) * 4) << 2)).xyz;
+				uint vertexID = ((id / 3u) / subtriangles) * 3u + (id % 3u);
+				switch(vertexID % 3)
+				{
+					case 0u:
+						if ((column & 1u) != 0)
+						{
+							v += incuv, u -= incuv;
+						}
+						break;
+					case 1u:
+						if ((column & 1u) == 0)
+						{
+							v += incuv, u -= incuv;
+						}
+						else
+						{
+							v += incuv, u -= incuv;
+							w += incuv, u -= incuv;
+						}
+						break;
+					case 2u:
+						if ((column & 1u) == 0)
+						{
+							u -= incuv, w += incuv;
+						}
+						else 
+						{
+							w += incuv, u -= incuv;
+						}
+						break;
+				}
+				return UnityObjectToClipPos(float4(u * p1 + v * p2 + w * p3, 1.0));
+			}
+
+			float4 PSMain (float4 vertex : SV_POSITION) : SV_TARGET
+			{
+				return (float4) 1.0;
+			}
+			ENDCG
+		}
+	}
+}
