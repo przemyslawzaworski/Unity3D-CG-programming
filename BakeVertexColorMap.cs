abeb8ad216767fef0cfe8814e585c511b5fae2ad
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2021-12-02 22:39:33 +0100

    Bake Vertex Colors To Texture

---
+++ BakeVertexColorMap.cs
@@ -0,0 +1,32 @@
+﻿// https://forum.unity.com/threads/is-it-possible-to-convert-mesh-vertex-colors-to-texture.1087835/#post-7010599
+using UnityEngine;
+ 
+public class BakeVertexColorMap : MonoBehaviour
+{
+    public Mesh SourceMesh;
+    public Shader BakeVertexColorMapShader;
+    public int Resolution = 2048;
+ 
+    void Start()
+    {
+        if (SourceMesh != null)
+        {
+            RenderTexture renderTexture = new RenderTexture(Resolution, Resolution, 0);
+            renderTexture.Create();
+            Material material = new Material(BakeVertexColorMapShader);
+            RenderTexture currentTexture = RenderTexture.active;
+            RenderTexture.active = renderTexture;
+            GL.Clear(false, true, Color.black, 1.0f);
+            material.SetPass(0);
+            Graphics.DrawMeshNow(SourceMesh, Vector3.zero, Quaternion.identity);
+            Texture2D texture = new Texture2D(Resolution, Resolution, TextureFormat.ARGB32, false);
+            texture.ReadPixels( new Rect(0, 0, Resolution, Resolution), 0, 0);
+            RenderTexture.active = currentTexture;
+            byte[] bytes = texture.EncodeToPNG();
+            System.IO.File.WriteAllBytes(System.IO.Path.Combine(Application.dataPath, "VertexColors.png"), bytes);
+            Destroy(material);
+            Destroy(texture);
+            renderTexture.Release();
+        }
+    }
+}
---
+++ BakeVertexColorMap.shader
@@ -0,0 +1,29 @@
+﻿Shader "Bake Vertex Color Map"
+{
+    SubShader
+    {
+        Pass
+        {
+            ZTest Off
+            ZWrite Off
+            Cull Off
+            CGPROGRAM
+            #pragma vertex VSMain
+            #pragma fragment PSMain
+ 
+            void VSMain (inout float4 vertex:POSITION, inout float2 uv:TEXCOORD0, inout float4 color:COLOR)
+            {
+                float2 texcoord = uv.xy;
+                texcoord.y = 1.0 - texcoord.y;
+                texcoord = texcoord * 2.0 - 1.0;
+                vertex = float4(texcoord, 0.0, 1.0);
+            }
+ 
+            float4 PSMain (float4 vertex:POSITION, float2 uv:TEXCOORD0, float4 color:COLOR) : SV_TARGET
+            {
+                return color;
+            }
+            ENDCG
+        }
+    }
+}
