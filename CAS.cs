bae8c75001bf540f47ef8baeaddab2e9d95c673c
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2022-01-18 18:11:12 +0100

    Contrast Adaptive Sharpening post fx effect

---
+++ CAS.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+// Add script to camera and assign Hidden/CAS shader
+public class CAS : MonoBehaviour 
+{
+	[Header("Contrast Adaptive Sharpening post processing effect")]
+	public Shader ContrastAdaptiveSharpeningShader;
+	[Range(0.0f, 1.0f)] public float Amount = 0.2f;
+	[Range(0.0f, 5.0f)] public float Radius = 1.0f;
+	public bool InvertY = false;
+	private Material _Material;
+
+	void Start()
+	{
+		if (ContrastAdaptiveSharpeningShader == null) ContrastAdaptiveSharpeningShader = Shader.Find("Hidden/CAS");
+		_Material = new Material(ContrastAdaptiveSharpeningShader);
+	}
+
+	void Blit(RenderTexture source, RenderTexture destination, Material mat)
+	{
+		RenderTexture.active = destination;
+		mat.SetTexture("_MainTex", source);
+		GL.PushMatrix();
+		GL.LoadOrtho();
+		GL.invertCulling = true;
+		mat.SetPass(0);
+		GL.Begin(GL.QUADS);
+		GL.MultiTexCoord2(0, 0.0f, 0.0f);
+		GL.Vertex3(0.0f, 0.0f, 0.0f);
+		GL.MultiTexCoord2(0, 1.0f, 0.0f);
+		GL.Vertex3(1.0f, 0.0f, 0.0f); 
+		GL.MultiTexCoord2(0, 1.0f, 1.0f);
+		GL.Vertex3(1.0f, 1.0f, 0.0f); 
+		GL.MultiTexCoord2(0, 0.0f, 1.0f);
+		GL.Vertex3(0.0f, 1.0f, 0.0f);
+		GL.End();
+		GL.invertCulling = false;
+		GL.PopMatrix();
+	}
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination) 
+	{
+		_Material.SetFloat("_Amount", Amount);
+		_Material.SetFloat("_Radius", Radius);
+		_Material.SetFloat("_InvertY", System.Convert.ToSingle(InvertY));
+		Blit (source, destination, _Material);
+	}
+
+	void OnDestroy()
+	{
+		if (_Material != null) Destroy(_Material);
+	}
+}
---
+++ CAS.shader
@@ -0,0 +1,46 @@
+﻿Shader "Hidden/CAS"
+{
+	Subshader
+	{
+		Pass
+		{
+			Cull Off
+			CGPROGRAM
+			#pragma vertex VSMain
+			#pragma fragment PSMain
+			#pragma target 5.0
+
+			Texture2D _MainTex;
+			float _Amount, _Radius, _InvertY;
+
+			// Contrast Adaptive Sharpening (CAS)
+			// Reference: Lou Kramer, FidelityFX CAS, AMD Developer Day 2019,
+			// https://gpuopen.com/wp-content/uploads/2019/07/FidelityFX-CAS.pptx
+			float3 ContrastAdaptiveSharpening (Texture2D tex, int2 texcoord, float knob, float radius)
+			{
+				float3 a = tex.Load(int3(texcoord + int2( 0.0, -1.0) * radius, 0)).rgb;
+				float3 b = tex.Load(int3(texcoord + int2(-1.0,  0.0) * radius, 0)).rgb;
+				float3 c = tex.Load(int3(texcoord + int2( 0.0,  0.0) * radius, 0)).rgb;
+				float3 d = tex.Load(int3(texcoord + int2( 1.0,  0.0) * radius, 0)).rgb;
+				float3 e = tex.Load(int3(texcoord + int2( 0.0,  1.0) * radius, 0)).rgb;
+				float m = min(a.g, min(b.g, min(c.g, min(d.g, e.g))));
+				float n = max(a.g, max(b.g, max(c.g, max(d.g, e.g))));
+				float w = sqrt(min(1.0 - n, m) / n) * lerp(-0.125, -0.2, knob);
+				return (w * (a + b + d + e) + c) / (4.0 * w + 1.0);
+			}
+
+			float4 VSMain (float4 vertex : POSITION) : SV_POSITION
+			{
+				return UnityObjectToClipPos(vertex);
+			}
+
+			float4 PSMain (float4 vertex : SV_POSITION) : SV_Target
+			{
+				int2 uv = int2(vertex.xy);
+				if (_InvertY > 0.5) uv.y = _ScreenParams.y - uv.y - 1;
+				return float4(ContrastAdaptiveSharpening(_MainTex, uv, _Amount, _Radius), 1.0);
+			}
+			ENDCG
+		}
+	}
+}
