bea5dfa3780aa9f1d904346236d223001b5bd41a
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2023-04-17 23:13:42 +0200

    AI player detection

---
+++ VisualAI.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualAI : MonoBehaviour
+{
+	[SerializeField] ComputeShader _ComputeShader;
+	[SerializeField] GameObject _Player;
+	[SerializeField] GameObject[] _Enemies;
+	[SerializeField] int _Delay = 1;
+
+	Camera[] _Cameras;
+	ComputeBuffer _CounterBuffer, _IndirectBuffer;
+	RenderTexture _RenderTexture, _TextureArray;
+	GUIStyle _GUIStyle = new GUIStyle();
+	float[] _Percents;
+	uint[] _Data = new uint[] {0};
+
+	void Start()
+	{
+		_Cameras = new Camera[_Enemies.Length];
+		_Percents = new float[_Enemies.Length];
+		for (int i = 0; i < _Cameras.Length; i++)
+		{
+			_Cameras[i] = _Enemies[i].AddComponent<Camera>();
+			_Cameras[i].depthTextureMode = DepthTextureMode.Depth;
+			_Cameras[i].farClipPlane = 150f;
+			_Cameras[i].fieldOfView = 40f;
+			_Cameras[i].renderingPath = RenderingPath.DeferredShading;
+			_Cameras[i].enabled = false;
+		}
+		_RenderTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.Depth);
+		_RenderTexture.Create();
+		_TextureArray = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.Depth);
+		_TextureArray.dimension = UnityEngine.Rendering.TextureDimension.Tex2DArray;
+		_TextureArray.volumeDepth = 2;
+		_TextureArray.Create();	
+		_CounterBuffer = new ComputeBuffer(1, sizeof(uint), ComputeBufferType.Counter);
+		_IndirectBuffer = new ComputeBuffer(1, sizeof(uint), ComputeBufferType.IndirectArguments);
+		_IndirectBuffer.SetData(_Data);
+		_GUIStyle.fontSize = 32;
+		_GUIStyle.normal.textColor = Color.white;
+	}
+
+	void GetVisibilityPercentage()
+	{
+		int index = Time.frameCount % _Enemies.Length;
+		_Cameras[index].enabled = true;
+		_ComputeShader.SetBuffer(0, "_CounterBuffer", _CounterBuffer);
+		_CounterBuffer.SetCounterValue(0);
+		_Cameras[index].targetTexture = _RenderTexture;
+		_Cameras[index].cullingMask = (1 << _Player.layer); // render only player
+		_Cameras[index].Render();
+		Graphics.CopyTexture(_RenderTexture, 0, 0, _TextureArray, 0, 0);
+		_Cameras[index].cullingMask = System.Int32.MaxValue & ~(1 << _Player.layer); // everything except player
+		_Cameras[index].Render();
+		Graphics.CopyTexture(_RenderTexture, 0, 0, _TextureArray, 1, 0);
+		_ComputeShader.SetTexture(0, "_TextureArray", _TextureArray);
+		_ComputeShader.Dispatch(0, Mathf.Max(1, _RenderTexture.width / 8), Mathf.Max(1, _RenderTexture.height / 8), 1);
+		_Cameras[index].enabled = false;
+		ComputeBuffer.CopyCount(_CounterBuffer, _IndirectBuffer, 0);
+		_IndirectBuffer.GetData(_Data);
+		float pixels = (float) (_RenderTexture.width * _RenderTexture.height);
+		_Percents[index] = _Data[0] / pixels * 100f;
+	}
+
+	void Update()
+	{
+		if (Time.frameCount % _Delay == 0)
+		{
+			GetVisibilityPercentage();
+		}
+	}
+
+	void OnGUI()
+	{
+		for (int i = 0; i < _Cameras.Length; i++)
+		{
+			GUI.Label(new Rect(0, i * 50, 200, 50), _Cameras[i].name + " : " + _Percents[i].ToString("N2"), _GUIStyle);
+		}
+	}
+
+	void OnDestroy()
+	{
+		if (_CounterBuffer != null) _CounterBuffer.Release();
+		if (_IndirectBuffer != null) _IndirectBuffer.Release();
+		if (_RenderTexture != null) _RenderTexture.Release();
+		if (_TextureArray != null) _TextureArray.Release();
+	}
+}
